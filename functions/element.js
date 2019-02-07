679b9a36c8a5edc1e1459190619bb8191e7de50e
Jinzhe Zeng <njzjz@qq.com>
2019-02-08 02:19:50 +0800

    bugfix

--- functions/element.js
+++ functions/element.js
@@ -9,7 +9,7 @@ var searchelement = function (input) {
         var inputpinyin = ''
     }
     for (var i in elementinfo) {
-        info = elementinfo[i];
+        var info = elementinfo[i];
         if (info.name == input || info.number == input || info.symbol.toLowerCase() == input || info.iupac.toLowerCase() == input || info.pinyin.toLowerCase() == input || info.pinyin == inputpinyin) {
             return info;
         }
