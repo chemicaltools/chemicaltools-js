605c4aa5e6620367f120e22babe17f6f992dd381
Jinzhe Zeng <njzjz@qq.com>
2019-04-07 02:46:14 +0800

    fix: bugfix

--- src/functions/mass.js
+++ src/functions/mass.js
@@ -1,13 +1,12 @@
 var elementinfo = require("../info/elementinfo");
 
 var calasc = function (x) {
-    var c = x[0];
-    var n = c.charCodeAt();
-    if (64 < n < 91)
+    var n = x[0].charCodeAt();
+    if (n > 64 && n < 91)
         return 1;
-    else if (96 < n < 123)
+    else if (n > 96 && n < 123)
         return 2;
-    else if (47 < n < 58)
+    else if (n > 47 && n < 58)
         return 3;
     else if (n == 40 | n == 91 | n == -23640)
         return 4;
@@ -30,27 +29,24 @@ var getNumber = function (x, index){
         num += x[index];
         index++;
     }
-    return {n: Math.max(parseInt(num), 1), l:num.length}
+    return {n: parseInt(num)||1, l:num.length}
 }
 
 var calculateMass = function (x) {
-    var output;
     var l = x.length;
     var s = 0;
     var m = 0;
     var massPer = new Array();
-    var atomnumber = new Int8Array(118);
+    var atomnumber = new Array(118);
     var mulnumber = new Array();
     var mulif = new Array();
     var mulleft = new Array();
     var mulright = new Array();
     var mulnum = new Array();
-    var n, i2, c, y1, y2, y3, y4, T;
     if (l > 0) {
-        for (var i = 1; i <= l; i++) {
-            i++;
+        for (var i = 0; i < l; i++) {
             mulnumber[i] = 1;
-            y1 = x[i-1];
+            y1 = x[i];
             if (calasc(y1) == 4)
                 mulif[i] = 1;
             else if (calasc(y1) == 5)
@@ -60,60 +56,42 @@ var calculateMass = function (x) {
             s += mulif[i];
         }
         if (s == 0) {
-            var n = 0;
-            for (var i = 1; i < l; i++) {
+            var nn = 0;
+            for (var i = 0; i < l; i++) {
                 if (mulif[i] == 1) {
-                    n++;
                     var c = 1;
                     var i2 = i + 1;
-                    mulleft[n] = i;
+                    mulleft[nn] = i;
                     while (c > 0) {
                         c += mulif[i2];
                         i2++;
                     }
-                    i2--;
-                    mulright[n] = i2;
-                    mulnum[n] = getNumber(x, i2).n;
+                    mulright[nn] = i2;
+                    mulnum[nn] = getNumber(x, i2).n;
+                    nn++;
                 }
             }
-            for (var i = 1; i <= n; i++) {
-                for (var i2 = mulleft[i]; i2 <= mulright[i]; i2++)
+            for (var i = 0; i < nn; i++) {
+                for (var i2 = mulleft[i]; i2 < mulright[i]; i2++)
                     mulnumber[i2] *= mulnum[i];
             }
-            for (var i = 1; i <= l; i++) {
-                y1 = x[i - 1];
+            for (var i = 0; i < l; i++) {
+                var y1 = x[i];
                 if (calasc(y1) == 1) {
-                    if (i >= l)
-                        y2 = "1";
-                    else
-                        y2 = x[i];
+                    var y2 = x[i+1] || "1";
+					var i2 = i;
                     if (calasc(y2) == 2) {
-                        n = elementchoose(y1 + y2);
-                        if (n != -1) {
-                            num = getNumber(x, i + 1);
-                            atomnumber[n] += num.n * mulnumber[i];
-                            i += 1+ num.l;
-                        }
-                    } else if (calasc(y2) == 3) {
-                        n = elementchoose(y1);
-                        if (n != -1) {
-                            num = getNumber(x, i);
-                            atomnumber[n] += num.n * mulnumber[i];
-                            i += num.l;
-                        }
-                    } else {
-                        n = elementchoose(y1);
-                        if (n != -1)
-                            atomnumber[n] += mulnumber[i];
-                    }
+						y1+=y2;
+						i++;
+                    } 
+                    var n = elementchoose(y1);
+                    if (n != -1){
+						num = getNumber(x, i+1);
+                        atomnumber[n] += num.n * mulnumber[i2];
+						i += num.l;
+					}
                 } else if (calasc(y1) == 5) {
-                    if (i >= l)
-                        y2 = "a";
-                    else
-                        y2 = x[i];
-                    if (calasc(y2) == 3) {
-                        i += getNumber(x, i).l;
-                    }
+                    i += getNumber(x, i+1).l;
                 }
             }
             for (i = 0; i < 118; i++) {
