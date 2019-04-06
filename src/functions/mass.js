8bbb40417c0b8321a6745a05b959618bb5955d20
Jinzhe Zeng <jzzeng@stu.ecnu.edu.cn>
2019-04-07 01:38:45 +0800

    Update mass.js

--- src/functions/mass.js
+++ src/functions/mass.js
@@ -24,10 +24,18 @@ var elementchoose = function (x) {
     }
     return -1;
 }
+var getNumber = function (x, index){
+    var num = "";
+    while (index < x.length && calasc(x[index]) == 3){
+        num += x[index];
+        index++;
+    }
+    return {n: Math.max(parseInt(num), 1), l:num.length}
+}
+
 var calculateMass = function (x) {
     var output;
     var l = x.length;
-    var i = 0;
     var s = 0;
     var m = 0;
     var massPer = new Array();
@@ -39,7 +47,7 @@ var calculateMass = function (x) {
     var mulnum = new Array();
     var n, i2, c, y1, y2, y3, y4, T;
     if (l > 0) {
-        while (i < l) {
+        for (var i = 1; i <= l; i++) {
             i++;
             mulnumber[i] = 1;
             y1 = x[i-1];
@@ -52,9 +60,8 @@ var calculateMass = function (x) {
             s += mulif[i];
         }
         if (s == 0) {
-            i = 1;
             var n = 0;
-            while (i < l) {
+            for (var i = 1; i < l; i++) {
                 if (mulif[i] == 1) {
                     n++;
                     var c = 1;
@@ -66,24 +73,14 @@ var calculateMass = function (x) {
                     }
                     i2--;
                     mulright[n] = i2;
-                    var num = "";
-                    while (i2 < l and calasc(x[i2]) == 3){
-                        num += x[i2];
-                        i2++;
-                    }
-                    mulnum[n] = Math.max(parseInt(num), 1)
+                    mulnum[n] = getNumber(x, i2).n;
                 }
-                i++;
             }
-            i = 0;
-            while (i < n) {
-                i++;
+            for (var i = 1; i <= n; i++) {
                 for (var i2 = mulleft[i]; i2 <= mulright[i]; i2++)
                     mulnumber[i2] *= mulnum[i];
             }
-            i = 0;
-            while (i < l) {
-                i++;
+            for (var i = 1; i <= l; i++) {
                 y1 = x[i - 1];
                 if (calasc(y1) == 1) {
                     if (i >= l)
@@ -91,43 +88,18 @@ var calculateMass = function (x) {
                     else
                         y2 = x[i];
                     if (calasc(y2) == 2) {
-                        T = y1 + y2;
-                        n = elementchoose(T);
+                        n = elementchoose(y1 + y2);
                         if (n != -1) {
-                            if (i + 1 >= l)
-                                y3 = "1";
-                            else
-                                y3 = x[i + 1];
-                            if (calasc(y3) == 3) {
-                                if (i + 2 >= l)
-                                    y4 = "a";
-                                else
-                                    y4 = x[i + 2]);
-                                if (calasc(y4) == 3) {
-                                    atomnumber[n] += parseInt(y3 + y4) * mulnumber[i];
-                                    i += 3;
-                                } else {
-                                    atomnumber[n] += parseInt(y3) * mulnumber[i]
-                                    i += 2;
-                                }
-                            } else {
-                                atomnumber[n] += mulnumber[i];
-                                i++;
-                            }
+                            num = getNumber(x, i + 1);
+                            atomnumber[n] += num.n * mulnumber[i];
+                            i += 1+ num.l;
                         }
                     } else if (calasc(y2) == 3) {
                         n = elementchoose(y1);
                         if (n != -1) {
-                            if (i + 1 >= l)
-                                y3 = "a";
-                            else
-                                y3 = x[i + 1];
-                            if (calasc(y3) == 3) {
-                                atomnumber[n] += parseInt(y2 + y3) * mulnumber[i];
-                                i += 2;
-                            } else {
-                                atomnumber[n] += parseInt(y2) * mulnumber[i];
-                            }
+                            num = getNumber(x, i);
+                            atomnumber[n] += num.n * mulnumber[i];
+                            i += num.l;
                         }
                     } else {
                         n = elementchoose(y1);
@@ -140,12 +112,7 @@ var calculateMass = function (x) {
                     else
                         y2 = x[i];
                     if (calasc(y2) == 3) {
-                        if (i + 1 >= l)
-                            y2 = "a";
-                        else
-                            y3 = x[i + 1];
-                        if (calasc(y3) == 3) i++;
-                        i++;
+                        i += getNumber(x, i).l;
                     }
                 }
             }
