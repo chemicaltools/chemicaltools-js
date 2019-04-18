d35fcf84500885567248ba73583949bcf980e1d7
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2019-04-19 02:08:37 +0800

    Update element.js

--- src/functions/element.js
+++ src/functions/element.js
@@ -1,20 +1,14 @@
 var pinyin = require("tiny-pinyin");
-var elementinfo = require("../info/elementinfo");;
-
+var elementinfomodule = require("../info/elementinfo");
+var elementinfo = elementinfomodule.elementinfo
+var search = elementinfomodule.search
 var searchelement = function (input) {
-    input = String(input).toLowerCase()
     try {
         var inputpinyin = pinyin.convertToPinyin(input).toLowerCase()
     } catch (e) {
         var inputpinyin = ''
     }
-    for (var i in elementinfo) {
-        var info = elementinfo[i];
-        if (info.name == input || info.number == input || info.symbol.toLowerCase() == input || info.iupac.toLowerCase() == input || info.pinyin.toLowerCase() == input || info.pinyin == inputpinyin) {
-            return info;
-        }
-    }
-    return null;
+    return search("name",input)||search("number",input)||search("symbol",input) || search("iupac",input)||search("pinyin",input)||search("pinyin",inputpinyin)
 }
 
 module.exports = searchelement;
